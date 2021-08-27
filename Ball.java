c2dac40d0b331aa98488ed64448c8fb762e74188
hugocostav <hugocostam100@outlook.com.br>
2021-08-27 12:43:25 -0300

    finished ball class

--- Ball.java
+++ Ball.java
@@ -24,5 +24,53 @@ public class Ball {
         dy = Math.sin(Math.toRadians(angle));
     }
 
-    
+    public void tick() {
+
+        if (x + (dx * speed) + width >= Game.WIDTH) {
+            dx *= -1;
+        } else if (x + (dx * speed) < 0) {
+            dx *= -1;
+        }
+
+        if (y >= Game.HEIGHT) {
+            // Enemy Point
+            System.out.println("Enemy Point");
+            new Game();
+            return;
+        } else if (y < 0) {
+            // Our Point
+            System.out.println("Our Point");
+            new Game();
+            return;
+        }
+
+        Rectangle bounds = new Rectangle((int) (x + (dx * speed)), (int) (y + (dy * speed)), width, height);
+
+        Rectangle boundsPlayer = new Rectangle(Game.player.x, Game.player.y, Game.player.width, Game.player.height);
+        Rectangle boundsEnemy = new Rectangle((int) Game.enemy.x, (int) Game.enemy.y, Game.enemy.width,
+                Game.enemy.height);
+
+        if (bounds.intersects(boundsPlayer)) {
+            int angle = new Random().nextInt(120 - 45) + 45 + 1;
+            dx = Math.cos(Math.toRadians(angle));
+            dy = Math.sin(Math.toRadians(angle));
+            if (dy > 0)
+                dy *= -1;
+        } else if (bounds.intersects(boundsEnemy)) {
+            int angle = new Random().nextInt(120 - 45) + 45 + 1;
+            dx = Math.cos(Math.toRadians(angle));
+            dy = Math.sin(Math.toRadians(angle));
+            if (dy < 0)
+                dy *= -1;
+        }
+
+        x += dx * speed;
+        y += dy * speed;
+
+    }
+
+    public void render(Graphics g) {
+        g.setColor(Color.yellow);
+        g.fillRect((int)x, (int)y, width, height);
+    }
 }

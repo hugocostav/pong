75bac9d96a233170a95bcf3374bddb7fe46df4c7
hugocostav <hugocostam100@outlook.com.br>
2021-08-27 12:34:06 -0300

    added ball contructor to calculate angle

--- Ball.java
+++ Ball.java
@@ -7,5 +7,22 @@ import java.util.Random;
 
 public class Ball {
 
+    public double x, y;
+    public int width, height;
+
+    public double dx, dy;
+    public double speed = 1.7;
+
+    public Ball(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.width = 4;
+        this.height = 4;
+
+        int angle = new Random().nextInt(120 - 45) + 45 + 1;
+        dx = Math.cos(Math.toRadians(angle));
+        dy = Math.sin(Math.toRadians(angle));
+    }
+
     
 }
